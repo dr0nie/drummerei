7926f2d9fe10d060dcebdada82018b2e56d9d671
Marcel <6847936+dr0nie@users.noreply.github.com>
2025-01-22 22:05:27 +0100

    fix: add unit tests for models and fix problems

--- drummerei/models.py
+++ drummerei/models.py
@@ -1,72 +1,8 @@
 from random import randint
-from datetime import time, timedelta,datetime
-from typing import Iterable, Optional
-from django.db import models
+from datetime import time, timedelta, datetime
 import qrcode
 
-
-def generate_start_time():
-    return Settings.load().default_start_time
-
-class Slot(models.Model):
-    name = models.CharField(max_length=255)
-    start_time = models.TimeField(default=generate_start_time)
-    slot_id = models.UUIDField(null=True,blank=True)
-
-    def __str__(self) -> str:
-        return f"{self.schedule_set.all().first()} @ {self.start_time}: {self.name}"
-
-def generate_qrcode(pin:int):
-    data = f"https://{Settings.load().url}?pin={pin}"
-    img = qrcode.make(data)
-    img.save("drummerei/static/image/qr.png")
-
-def generate_pin() -> int:
-    pin = randint(1000,9999)
-    generate_qrcode(pin)
-    return pin
-
-def generate_slots():
-    number_of_slots = int(
-        Settings.load().default_duration.total_seconds() / Settings.load().default_slot_duration.total_seconds()
-    )
-    slots = []
-    for i in range(number_of_slots):
-        slot = Slot(
-                start_time=(
-                    datetime.combine(
-                        datetime(1,1,1),
-                        generate_start_time()
-                    ) + timedelta(minutes=i*30)
-                    ).time()
-                )
-        slot.save()
-        slots.append(slot)
-    return slots
-
-#TODO: use signals after save
-class Schedule(models.Model):
-    slots = models.ManyToManyField(Slot,default=generate_slots,null=True,blank=True)
-    pin = models.IntegerField(default=generate_pin)
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
-
-    def delete(self):
-        for slot in list(self.slots.all()):
-            slot.delete()
-        super().delete()
-
-    def save(self):
-        schedules=list(self.__class__.objects.filter(id=self.id))
-        if len(schedules) == 1:
-            old_schedule = schedules[0]
-            oldpin = old_schedule.pin
-            if self.pin != oldpin:
-                generate_qrcode(self.pin)
-        super().save()
-
-    def __str__(self) -> str:
-        return str(self.start_time.date())
+from django.db import models
 
 class Settings(models.Model):
     class Meta:
@@ -80,14 +16,111 @@ class Settings(models.Model):
     default_duration = models.DurationField(default=timedelta(hours=4))
     default_slot_duration = models.DurationField(default=timedelta(minutes=30))
 
-    def save(self,*args, **kwargs):
-        self.pk=1
-        super().save(*args, **kwargs)
-
     @classmethod
     def load(cls):
         obj, _ = cls.objects.get_or_create(id=1)
         return obj
     
+    def save(self,*args, **kwargs):
+        self.pk=1
+        super().save(*args, **kwargs)
+
     def __str__(self) -> str:
         return self.title
+
+
+def generate_start_time() -> time:
+    """
+    Retrieves the default start time from the Settings model.
+
+    This function loads the settings and returns the default start time specified
+    in the Settings model. It is used to initialize time-related fields with a 
+    predefined default value.
+
+    Returns:
+        time: The default start time as specified in the Settings model.
+    """
+    return Settings.load().default_start_time
+
+
+class Slot(models.Model):
+    name = models.CharField(max_length=255)
+    start_time = models.TimeField(default=generate_start_time)
+    slot_id = models.UUIDField(null=True,blank=True)
+
+    def __str__(self) -> str:
+        schedule = None
+        all_schedules = Schedule.objects.all()
+        if all_schedules.exists():
+            schedule = all_schedules.first()
+        return f"{schedule} @ {self.start_time}: {self.name}"
+
+def generate_pin() -> int:
+    """
+    Generates a random 4-digit pin.
+
+    This function generates a random integer between 1000 and 9999 to be used
+    as a pin.
+
+    Returns:
+        int: A randomly generated 4-digit pin.
+    """
+    pin = randint(1000,9999)
+    return pin
+
+class Schedule(models.Model):
+    slots = models.ManyToManyField(Slot,editable=False)
+    pin = models.IntegerField(default=generate_pin)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+    def save(self):
+        #TODO: put qrcode path in Settings
+        qrcode_path = "drummerei/static/image/qr.png"
+        if self.id is None:
+            self.generate_qrcode(qrcode_path)
+            super().save()            
+            self.slots.add(*self.__generate_slots())
+
+        else:
+            schedules=list(self.__class__.objects.filter(id=self.id))
+            old_schedule = schedules[0]
+            oldpin = old_schedule.pin
+            if self.pin != oldpin:
+                self.generate_qrcode(qrcode_path)
+
+        return super().save()
+
+    def delete(self):
+        for slot in list(self.slots.all()):
+            slot.delete()
+        super().delete()
+
+    def generate_url_with_pin(self):
+        return f"{Settings.load().url}?pin={self.pin}"
+
+    def generate_qrcode(self,path:str):
+        data = self.generate_url_with_pin()
+        img = qrcode.make(data)
+        img.save(path)
+
+    def __str__(self) -> str:
+        return str(self.start_time.date())
+    
+    def __generate_slots(self) -> list[Slot]:
+        number_of_slots = int(
+            Settings.load().default_duration.total_seconds() / Settings.load().default_slot_duration.total_seconds()
+        )
+        slots = []
+        for i in range(number_of_slots):
+            slot = Slot(
+                    start_time=(
+                        datetime.combine(
+                            datetime(1,1,1),
+                            Settings.load().default_start_time
+                        ) + timedelta(minutes=i*30)
+                        ).time()
+                    )
+            slot.save()
+            slots.append(slot)
+        return slots
--- drummerei/tests.py
+++ drummerei/tests.py
@@ -2,11 +2,20 @@ from datetime import datetime, timedelta
 
 from django.test import TestCase
 
-from .models import Settings, Schedule, Slot
+from .models import Settings, Schedule, Slot,generate_pin, generate_start_time
 
+class HelperFunctionsTest(TestCase):
+    def test_generate_pin(self):
+        self.assertTrue(1000 <= generate_pin() <= 9999 ) # ! pin is RANDOM
+    def test_generate_default_start_time(self):
+        self.assertEqual(generate_start_time() , Settings.load().default_start_time)
+        
 class SettingsModelTest(TestCase):
     def setUp(self):
         self.settings = Settings.load()
+
+    def test_singleton(self):
+        self.assertEqual(self.settings , Settings.load())
 
     def test_settings_creation(self):
         self.assertEqual(self.settings.title, 'Drummerei')
@@ -19,31 +28,48 @@ class ScheduleModelTest(TestCase):
         self.start_time = datetime.now()
         self.end_time=self.start_time + timedelta(hours=4)
 
+        self.settings = Settings.load()
+        self.settings.url = "http://localhost:8000"
+        self.settings.save()
         self.schedule = Schedule(
             start_time=self.start_time,
             end_time=self.end_time,
         )
+        self.schedule.save()
+
+    def test_generate_url_with_pin(self):
+        self.assertEqual(
+            self.schedule.generate_url_with_pin(),
+            self.settings.url+"?pin="+str(self.schedule.pin)
+        )
+
+    def test_generate_qrcode(self):
+        self.assertTrue(False, 'Not implemented')
 
     def test_schedule_creation(self):
         self.assertEqual(self.schedule.start_time, self.start_time)
         self.assertEqual(self.schedule.end_time, self.end_time )
         self.assertTrue(isinstance(self.schedule, Schedule))
 
-        self.assertTrue(1000 <= self.schedule.pin <= 9999 ) # ! pin is RANDOM
+        self.assertTrue(0 < len(self.schedule.slots.all())  )
+        
         self.assertEqual(str(self.schedule), str(self.schedule.start_time.date()))
 
 class SlotModelTest(TestCase):
     def setUp(self):
         self.name = "TestDJ"
-        self.time = datetime.now()
+        self.start_time = datetime.now()
 
         self.slot = Slot(
             name=self.name,
-            time=self.time,
+            start_time=self.start_time,
         )
 
     def test_schedule_creation(self):
         self.assertEqual(self.slot.name, self.name)
-        self.assertEqual(self.slot.time, self.time)
+        self.assertEqual(self.slot.start_time, self.start_time)
         self.assertTrue(isinstance(self.slot, Slot))
-        self.assertEqual(str(self.slot), self.slot.name)
+        self.assertEqual(
+            str(self.slot),
+            f"None @ {self.slot.start_time}: {self.slot.name}"
+        )
