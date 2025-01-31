50d6aa5d4143113ab2f90e9ca05ddbab9e2c0840
Marcel <6847936+dr0nie@users.noreply.github.com>
2025-01-31 02:15:45 +0100

    fix: set api server url for schema

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,20 @@ from ninja import NinjaAPI
 from .views.schedules import router as schedule_router
 from .views.slots import router as slot_router
 
-app = NinjaAPI()
+app = NinjaAPI(
+    title="drummerei API",
+    version="0.1.0",
+    description="API for the drummerei app.",
+    servers=[
+        { "url": "http://localhost:8000" },
+    ],
+    openapi_extra={
+       "info": {
+           "termsOfService": "",
+       }
+   },
+)
+
 app.add_router("/schedules", schedule_router)
 app.add_router("/schedules/", slot_router)
 
